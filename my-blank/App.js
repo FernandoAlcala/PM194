f664f759996cdd71ca8fe5497149d9e68225c951
FernandoAlcala <166526484+FernandoAlcala@users.noreply.github.com>
2025-06-11 19:49:58 -0600

    Trabajo con estados de componentes

--- my-blank/App.js
+++ my-blank/App.js
@@ -2,11 +2,21 @@
 
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import React,{useState} from 'react';
 
-const Texto = (props) => {
-  const {contenido} = props 
+const Texto = () => {
+  const [contenido,setContenido] = useState('Hola Mundo React Native');
+  const actualizarTexto = () => {setContenido('Estado actualizado del text');}
   return (
-    <Text>{contenido}</Text>
+    <Text onPress={actualizarTexto}> {contenido} </Text>
+  );
+}
+
+const Botonazo = () => {
+  const [texto, setTexto] = useState('No aprietes el botón');
+  const actualizarBoton = () => { setTexto('😡'); };
+  return (
+    <Button title={texto} onPress={actualizarBoton}/>
   );
 }
 
@@ -16,13 +26,14 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Button title='Presioname'></Button>
-      <Texto contenido = "Hola"></Texto>
-      <Texto contenido = "Mundo"></Texto>
-      <Texto contenido = "React Native"></Texto>
+      <Botonazo></Botonazo>
+      <Texto></Texto>
+      <Texto></Texto>
+      <Texto></Texto>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 /* Zona 3: Estética*/
 const styles = StyleSheet.create({
